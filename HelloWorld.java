6e33851b526386eac7f3b2629abf549a0098fa7c
Marina <marina-radiskovic@outlook.com>
2018-07-19 13:50:02 +0200

    Revert "Added Ridiculous message" Reverting commit that adds a ridiculous message This reverts commit 279bf17aa9e31c2b4895294048a8d4d56db32edf.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ class HelloWorld{
 	public static void main (String[] args){ 
 		System.out.println("Hello World!"); 
 		System.out.printl("Guess who's back");
-		System.out.println("This is getting ridiculous...")
 	}
 }
